272c84604bfdfe47eefbd1cbd2a7752096d10628
Gennadii Miroshnichenko <genamister@gmail.com>
2017-06-14 18:16:25 +0300

    Подготовка базового класса

---
+++ src/test/java/com/mainacad/CustomerRegistratoinTests.java
@@ -0,0 +1,17 @@
+package com.mainacad;
+
+/**
+ * Created by Gennadii on 14-Jun-17.
+ */
+import org.junit.Test;
+
+public class CustomerRegistratoinTests extends TestBase {
+
+    @Test
+    public void canRegisterCustomerTest(){
+
+    }
+}
+
+
+
